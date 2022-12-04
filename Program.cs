04a88440247c1e8e5ebd51fd65dba194e0a97e58
Daniel_Khrustitsky <snowcatdx@gmail.com>
2022-12-04 12:43:45 +0200

    task 13 ... incomplete

--- Program.cs
+++ Program.cs
@@ -4,6 +4,8 @@
 // 782 -> 8 
 // 918 -> 1
 
+//-------------//
+
 // Console.Write("Введите трехзначное число: ");
 // int number = Convert.ToInt32(Console.ReadLine());
 // int firstNumber = number / 10;
@@ -11,8 +13,30 @@
 
 // Console.WriteLine("Вторая цифра: " + secondDigit);
 
+//-------------//
+
 // Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
 //Пример: 
 // 645 -> 5
 // 78 -> третьей цифры нет
 // 32679 -> 6
+
+int number = 645;
+int thirdDigit;
+if (number > 99 && number < 1000)
+{
+    while (number > 999)
+    {
+        number = number / 1000;
+    }
+    thirdDigit = number % 10;
+    Console.WriteLine(number);
+    Console.WriteLine(thirdDigit);
+}
+else
+{
+    Console.WriteLine("третьей цифры нет");
+}
+
+
+
